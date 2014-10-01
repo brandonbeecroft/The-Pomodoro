1bc55cdde9a53de516072d11414d1803b0cc660a
Brandon Beecroft <brandonbeecroft@gmail.com>
2014-10-01 17:36:22 -0600

    adds connected sliders for work time label

--- The Pomodoro/POSettingsViewController.m
+++ The Pomodoro/POSettingsViewController.m
@@ -10,6 +10,10 @@
 
 @interface POSettingsViewController ()
 
+@property (weak, nonatomic) IBOutlet UISlider *roundLengthSlider;
+@property (weak, nonatomic) IBOutlet UISlider *breakLengthSlider;
+@property (nonatomic, strong) NSArray *numbers;
+
 @end
 
 @implementation POSettingsViewController
@@ -21,9 +25,28 @@
     [self.navigationController.navigationBar setBarTintColor:[UIColor redColor]];
     [self.navigationController.navigationBar setTitleTextAttributes:@{NSFontAttributeName:[UIFont fontWithName:@"Avenir" size:20],
                                                                       NSForegroundColorAttributeName: [UIColor blackColor]}];
-    
+    _numbers = @[@(5), @(10), @(15), @(20), @(25), @(30)];
+    NSInteger numberOfSteps = [self.numbers count]-1;
+
+    NSLog(@"%li",(long)numberOfSteps);
+
+    _roundLengthSlider.maximumValue = numberOfSteps;
+    _roundLengthSlider.minimumValue = 0;
+    _breakLengthSlider.maximumValue = numberOfSteps;
+    _roundLengthSlider.minimumValue = 0;
+
+    _roundLengthSlider.continuous = YES;
+    _breakLengthSlider.continuous = YES;
+
 }
 
+- (IBAction)valueChanged:(id)sender {
+    NSInteger index = (NSInteger)(_roundLengthSlider.value + 0.5);
+    [_roundLengthSlider setValue:index animated:NO];
+    //[_breakLengthSlider setValue:index animated:NO];
+    NSNumber *number = _numbers[index];
+    NSLog(@"number: %@",number);
+}
 
 
 @end
